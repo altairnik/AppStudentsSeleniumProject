9689352122cafb23783fffca2af1be8c95c25c0f
altairnik <altairnik@gmail.com>
2021-04-21 22:31:28 +0300

    add fixes

--- src/test/java/AppStudents/AppStudentsTest.java
+++ src/test/java/AppStudents/AppStudentsTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.Test;
 
+import java.sql.Time;
 import java.time.Instant;
 import java.util.Date;
 import java.util.UUID;
@@ -15,6 +16,7 @@ public class AppStudentsTest {
     String uniqueName;
     String unixTimeStampPhone;
     long unixTimeStamp;
+    //long unixTimeStamp;
     @Test
     public void checkMethod(){
     System.setProperty("webdriver.chrome.driver","c:/testing/chromedriver.exe");
@@ -25,12 +27,14 @@ public class AppStudentsTest {
     driver.get("http://151.80.70.42:3000/");
     driver.findElement(By.cssSelector("i.mdi-content-add")).click();
     driver.findElement(By.cssSelector("div.main-content input#icon_prefix")).clear();
+    //driver.findElement(By.cssSelector("div.main-content input#icon_prefix")).sendKeys(UUID.randomUUID().toString());
     driver.findElement(By.cssSelector("div.main-content input#icon_prefix")).sendKeys(uniqueName);
     driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).clear();
+    //driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).sendKeys(((Long) System.currentTimeMillis()).toString());
     driver.findElement(By.cssSelector("div.main-content input#icon_telephone")).sendKeys(unixTimeStampPhone);
     driver.findElement(By.cssSelector("div.col>a.btn")).click();
     }
-//    just in case
+//    just in case)
 //    @AfterClass
 //    public void afterMethod(){
 //    driver.quit();
